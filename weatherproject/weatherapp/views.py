a7c6d7d8741b79dafd19bb728e2ca56e7c477893
milesba4 <milesbaffour@gmail.com>
2022-09-22 11:32:44 -0700

    ignoring secret key

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'weatherapp/index.html' )
+    welcome_message = "Hello, welcome to Weather App"
+    return render(request, 'weatherapp/index.html', {'welcome_message':welcome_message} )
--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x1lf(5zo*9cgm@x#ouy^swc24l1(p(_w_g#*&f)u8z$z_8h*bx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
