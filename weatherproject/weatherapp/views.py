4413f472292c85f19a957399644c90b414d6a1f9
Miles Baffour <milesbaffour@gmail.com>
2022-09-24 16:22:47 +0000

    adding sync test message

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+   str = "sync testing"
     welcome_message = "Hello, welcome to Weather App"
     return render(request, 'weatherapp/index.html', {'welcome_message':welcome_message} )
