300668e0d5cce1ee3f54c25f8dfd1153875c67b7
milesba4 <milesbaffour@gmail.com>
2022-09-29 00:10:34 -0700

    adding form for state and country

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -6,8 +6,26 @@ import datetime
 def index(request):
     api_key = config("API_KEY")
     welcome_message = "Hello, welcome to Weather App"
+
+    #checking input form and assigning input values to variables
+    if 'city' in request.POST:
+        city  = request.POST['city']
+    else:
+        city = 'Washington DC'
+    if 'state' in request.POST:
+        state = request.POST['state']
+    else:
+        state = 'DC '
+    if 'country' in request.POST:
+        country = request.POST['country']
+    else:
+        country = 'USA'
+
+
     URL = 'https://api.openweathermap.org/data/2.5/weather'
-    PARAMS = {'q': 'houston' + ',' + 'USA', 'appid': api_key}
+    PARAMS = {'q': city + ',' + state+ ',' + country, 'appid': api_key}
+
+
 
     #send req to API
     r = requests.get(url=URL,params=PARAMS)
@@ -19,4 +37,4 @@ def index(request):
     icon = response['weather'][0]['icon']
     temperature = response['main']["temp"]
     day = datetime.date.today()
-    return render(request, 'weatherapp/index.html', {'welcome_message':welcome_message, 'description': description, 'icon':icon, 'temperature':temperature, 'day': day})
+    return render(request, 'weatherapp/index.html', {'welcome_message':welcome_message, 'description': description, 'icon':icon, 'temperature':temperature, 'day': day, 'city': city, 'state': state, 'country': country})
